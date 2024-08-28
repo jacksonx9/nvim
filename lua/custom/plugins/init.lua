1c9cf4944bf88a94e64581efb7fcb8595c061020
Jackson Xie <jacksonxie@Jacksons-Air.hitronhub.home>
2024-08-27 17:03:51 -0700

    add color

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- color scheme
+  {
+    'norcalli/nvim-colorizer.lua',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    cmd = 'ColorizerToggle',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+}
